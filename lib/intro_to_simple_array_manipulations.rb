98f2c607f5fc2bf6fb68dfb3d9d712b4db41042f
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 00:27:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_pop(array)
 end
 
 def using_pop_with_args(array,num)
-  return array.pop(num)
+  return array.pop(2)
 end
 
 def using_delete(array,string)
@@ -22,12 +22,4 @@ def using_delete(array,string)
     end
     i += 1
   end
-end
-
-magic_exit_number = 7
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
 end
