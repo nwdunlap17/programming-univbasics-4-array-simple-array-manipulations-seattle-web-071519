aa61eaa6691d697cee2f1aad6a4716bd7046caae
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 00:24:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,10 +16,18 @@ end
 
 def using_delete(array,string)
   i = 0
-  while i < array.size do
+#  while i < array.size do
 #    if(array[i]==string)
 #       array.delete(i)
 #    end
     i++
-  end
+#  end
+end
+
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
 end
