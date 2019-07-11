33b9cc4d6ab8b3eebb29301b5f4645406052d210
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 00:25:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,10 +24,10 @@ end
 #  end
 #end
 
-#magic_exit_number = 7
-#count = 0
-#while count < 10 do
-#  break if count == magic_exit_number
-#  puts "I am the #{count}, I love to count!" # Work
-#  count = count + 1
-#end
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
